f9da8b0471f62e2b68d3760acec56516e59b14bc
tadiparChinese <pratiknagdeve@gmail.com>
2024-02-22 23:23:08 +0530

    prompt template modified

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_gemini_repsonse(input):
     response=model.generate_content(input)
     return response.text
 
-def input_pdf_text(uploaded_file):
+def input_pdf_to_text(uploaded_file):
     reader=pdf.PdfReader(uploaded_file)
     text=""
     for page in range(len(reader.pages)):
@@ -23,22 +23,6 @@ def input_pdf_text(uploaded_file):
     return text
 
 #Prompt Template
-
-# input_prompt="""
-# Hey Act Like a skilled or very experience ATS(Application Tracking System)
-# with a deep understanding of tech field,software engineering,data science ,data analyst
-# and big data engineer. Your task is to evaluate the resume based on the given job description.
-# You must consider the job market is very competitive and you should provide 
-# best assistance for improving thr resumes. Assign the percentage Matching based 
-# on Jd and
-# the missing keywords with high accuracy
-# resume:{text}
-# description:{jd}
-
-# I want the response in one single string having the structure
-# {{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
-# """
-# #####
 
 input_prompt="""
 
@@ -51,24 +35,11 @@ description: {jd}
 Expected response format:
 {{"JD Match": "%", "MissingKeywords": [], "Profile Summary": ""}}
 
-
-
 """
 
-
-
-
-
-
-
-
-
-
-
-
 ## streamlit app
-st.title("Smart ATS")
-st.text("Improve Your Resume ATS")
+st.title("Resume Analysis System")
+st.text("Check the Skill Gap Analysis")
 jd=st.text_area("Paste the Job Description")
 uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
 
@@ -76,6 +47,6 @@ submit = st.button("Submit")
 
 if submit:
     if uploaded_file is not None:
-        text=input_pdf_text(uploaded_file)
+        text=input_pdf_to_text(uploaded_file)
         response=get_gemini_repsonse(input_prompt)
         st.subheader(response)
